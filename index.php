e8e33e93802684c2d1dcb274f58e2196fa50b06c
JDgayagoy <jdgprofessional@gmail.com>
2024-11-19 20:20:32 +0800

    QR code Reader

--- index.php
+++ index.php
@@ -1,14 +1,35 @@
 <?php
-    include('php/cont.php');
+  include("php/cont.php")
 ?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="dist/output.css">
+  <title>Document</title>
+  <script src="./node_modules/html5-qrcode/html5-qrcode.min.js"></script>
 </head>
 <body>
-    
+  <div class=" w-500 h-500" id="reader">reader</div>
+  <script>
+
+    const scanner = new Html5QrcodeScanner('reader',{
+      qrbox:{
+        width:500,
+        height:500,
+      },
+      fps:20,
+    });
+
+    scanner.render(success, error);
+    function success(result){
+      console.log(result);
+      scanner.clear();
+      
+    }
+    function error(err){
+      console.error(err);
+    }
+  </script>
 </body>
 </html>
