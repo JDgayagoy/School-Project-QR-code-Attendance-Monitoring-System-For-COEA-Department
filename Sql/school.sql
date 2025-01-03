1b637f6a887af47f7365a98db20148883ab4ba54
JDgayagoy <jdgprofessional@gmail.com>
2025-01-03 12:09:56 +0800

    Database update

---
+++ Sql/school.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 03, 2025 at 05:08 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `course_code` varchar(10) NOT NULL,
+  `course_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `course_code`, `course_name`) VALUES
+(1, 'BSABE', 'BACHELOR OF SCIENCE IN AGRICULTURAL AND BIOSYSTEMS ENGINEERING'),
+(2, 'BSCHE', 'BACHELOR OF SCIENCE IN CHEMICAL ENGINEERING'),
+(3, 'BSCPE', 'BACHELOR OF SCIENCE IN COMPUTER ENGINEERING'),
+(4, 'BSECE', 'BACHELOR OF SCIENCE IN ELECTRONICS ENGINEERING'),
+(5, 'BSCE', 'BACHELOR OF SCIENCE IN CIVIL ENGINEERING'),
+(6, 'BSEE', 'BACHELOR OF SCIENCE IN ELECTRICAL ENGINEERING'),
+(7, 'BSGE', 'BACHELOR OF SCIENCE IN GEODETIC ENGINEERING'),
+(8, 'BSA', 'BACHELOR OF SCIENCE IN ARCHITECTURE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
+  `student_id` varchar(10) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `middle_initial` char(1) DEFAULT NULL,
+  `password` varchar(50) NOT NULL,
+  `year` int(11) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `section_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`id`, `student_id`, `last_name`, `first_name`, `middle_initial`, `password`, `year`, `course_id`, `section_id`) VALUES
+(5, '22-31928', 'Manuela', 'Katrina Anne', 'A', 'dabid', 4, 5, 13);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sections`
+--
+
+CREATE TABLE `sections` (
+  `id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `section` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sections`
+--
+
+INSERT INTO `sections` (`id`, `course_id`, `section`) VALUES
+(1, 1, 'A'),
+(2, 1, 'B'),
+(3, 1, 'C'),
+(4, 2, 'A'),
+(5, 2, 'B'),
+(6, 3, 'A'),
+(7, 3, 'B'),
+(8, 3, 'C'),
+(9, 3, 'D'),
+(10, 4, 'A'),
+(11, 4, 'B'),
+(12, 4, 'C'),
+(13, 5, 'A'),
+(14, 5, 'B'),
+(15, 5, 'C'),
+(16, 5, 'D'),
+(17, 5, 'E'),
+(18, 6, 'A'),
+(19, 6, 'B'),
+(20, 6, 'C'),
+(21, 7, 'A'),
+(22, 7, 'B'),
+(23, 8, 'A'),
+(24, 8, 'B'),
+(25, 8, 'C');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `student_id` varchar(10) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `middle_initial` char(1) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `year` int(11) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `section_id` int(11) DEFAULT NULL,
+  `image_path` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `student_id`, `last_name`, `first_name`, `middle_initial`, `password`, `year`, `course_id`, `section_id`, `image_path`) VALUES
+(13, '22-22237', 'Gayagoy', 'John David', 'M', '$2y$10$PANl0ncSRrSNidxOtcQkm.hQIcqanu2MH6BqxvW4ma/2D2KOOJwZC', 3, 2, 4, 'QR-Codes/22-22237.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sections`
+--
+ALTER TABLE `sections`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id` (`course_id`),
+  ADD KEY `section_id` (`section_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `sections`
+--
+ALTER TABLE `sections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `sections`
+--
+ALTER TABLE `sections`
+  ADD CONSTRAINT `sections_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`section_id`) REFERENCES `sections` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
